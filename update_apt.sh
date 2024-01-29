32a0976c54753de935de0d70a893a91f89e70744
Guy Marshall <guymarshall9@gmail.com>
2024-01-29 18:40:56 +0000

    UPDATE: Add flatpak update

--- update_apt.sh
+++ update_apt.sh
@@ -3,3 +3,9 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
+
+if command -v flatpak &> /dev/null; then
+    flatpak update
+else
+    echo "'flatpak' is not installed. Skipping update."
+fi
