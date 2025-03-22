05d4c5a4bf9a02e5ad2a7f8942457a2f15e1a5af
Guy Marshall <guymarshall9@gmail.com>
2025-03-22 23:07:40 +0000

    UPDATE: Remove google chrome from both aur scripts

--- install_aur_apps.sh
+++ install_aur_apps.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/google-chrome.git
-cd ~/google-chrome
-makepkg -si
-
-cd
-
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd ~/visual-studio-code-bin
 makepkg -si
--- update_aur_apps.sh
+++ update_aur_apps.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd ~/google-chrome
-git pull
-makepkg -si
-
-cd
-
 cd ~/visual-studio-code-bin
 git pull
 makepkg -si
