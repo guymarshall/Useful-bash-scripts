5ab6db9144a5c79d93eb03072ea37c94dc7b34c7
Guy Marshall <guymarshall9@gmail.com>
2025-04-08 17:49:02 +0100

    ADD: minecraft-launcher and atlauncher

--- install_aur_apps.sh
+++ install_aur_apps.sh
@@ -3,5 +3,14 @@
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd ~/visual-studio-code-bin
 makepkg -si
+cd
 
+git clone https://aur.archlinux.org/minecraft-launcher.git
+cd ~/minecraft-launcher
+makepkg -si
+cd
+
+git clone https://aur.archlinux.org/atlauncher.git
+cd ~/atlauncher
+makepkg -si
 cd
--- update_aur_apps.sh
+++ update_aur_apps.sh
@@ -3,5 +3,14 @@
 cd ~/visual-studio-code-bin
 git pull
 makepkg -si
+cd
 
+cd ~/minecraft-launcher
+git pull
+makepkg -si
+cd
+
+cd ~/atlauncher
+git pull
+makepkg -si
 cd
